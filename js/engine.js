2df3b8391ea677e00969c3233cfcb92c2fa61310
Luiz Guilherme <contato.guilhermealmeida@gmail.com>
2024-02-04 16:38:33 -0300

    Skills

--- js/engine.js
+++ js/engine.js
@@ -1,9 +1,8 @@
 const drops = document.querySelectorAll(".dropdown");
 
-
-drops.forEach(drop => {
-    drop.addEventListener("click",openDrop => {
-        drop.classList.toggle("open")
-    })      
+drops.forEach((drop) => {
+  drop.addEventListener("click", (openDrop) => {
+    drop.classList.toggle("open");
+  });
 });
 console.log(drops);
