0cfd89d0da99b6950601d6038f4e9bc16b64e94b
Luiz Guilherme <contato.guilhermealmeida@gmail.com>
2024-02-15 15:13:32 -0300

    List Lang Json file

--- js/main.js
+++ js/main.js
@@ -1,24 +1,32 @@
 const profile = {
-    photo: document.querySelector('.photo'),
-    job: document.querySelector('.contact'),
-    locate: document.querySelector('.locate'),
-    whats: document.querySelector('.whats'),
-    email: document.querySelector('.email')
+  photo: document.querySelector(".photo"),
+  job: document.querySelector(".contact"),
+  locate: document.querySelector(".locate"),
+  whats: document.querySelector(".whats"),
+  email: document.querySelector(".email"),
+};
+
+const langUl = document.getElementById("lang");
+
+async function listLanguages(profileData) {
+  let lang = profileData.languages;
+  lang.forEach((element) => {
+    const line = document.createElement("li");
+    line.innerHTML = element;
+    langUl.appendChild(line);
+  });
 }
 
-
-
-function updateProfileInfo(profileData){
-    profile.photo.src = profileData.photo
-    profile.job.innerHTML = profileData.job
-    profile.locate.innerHTML = profileData.location
-    profile.whats.innerHTML = profileData.phone
-    profile.email.innerHTML = profileData.email
-    
+function updateProfileInfo(profileData) {
+  profile.photo.src = profileData.photo;
+  profile.job.innerHTML = profileData.job;
+  profile.locate.innerHTML = profileData.location;
+  profile.whats.innerHTML = profileData.phone;
+  profile.email.innerHTML = profileData.email;
 }
 
-(async function init(){
-    const profileData = await fetchProfileData()
-    console.log(profileData)
-    updateProfileInfo(profileData)
-})()
+(async function init() {
+  const profileData = await fetchProfileData();
+  updateProfileInfo(profileData);
+  listLanguages(profileData);
+})();
